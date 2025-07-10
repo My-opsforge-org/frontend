9ae073d4ce7b05a3c1e93922ff6f21df50c97822
Aftab Khan <khanaftaba1@gmail.com>
2025-07-10 01:04:55 -0400

    deployment

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,1 @@
-// Common API base URL for Flask backend
-// eslint-disable-next-line no-undef
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api';
+export const API_BASE_URL = 'http://4.206.72.158:8000/api';
