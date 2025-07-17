fd287b43bbb7edbb1411b2235d8b357e305d115b
Aftab Khan <khanaftaba1@gmail.com>
2025-07-17 14:41:47 -0400

    trigger

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,3 @@
 // export const API_BASE_URL = 'http://localhost:5000/api';
-export const API_BASE_URL = 'http://4.206.17.251:8000/api';
+export const API_BASE_URL = 'http://4.204.56.217:8000/api';
 // export const API_BASE_URL = 'http://localhost:5000/api';
